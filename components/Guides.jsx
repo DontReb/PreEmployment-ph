25ffd36c5d0fdbcd391f69c7eaef76fdfb72202e
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-13 15:35:42 +0800

    Added the Guides content in the page.

---
+++ components/Guides.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Guides() {
+    return (
+        <section id="guides">
+            <div className="cont">
+                <h2>Guides</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default Guides;
