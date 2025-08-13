631a330f332ddbf2dc5112b4d813acc257601e5d
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-13 15:34:59 +0800

    Added the FAQs content in the page.

---
+++ components/FAQs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FAQs() {
+    return (
+        <section id="faqs">
+            <div className="cont">
+                <h2>FAQs</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default FAQs;
