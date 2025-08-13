592714286747259db1f7d81ebfe61898bbd5ebf6
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-13 15:36:17 +0800

    Added the Home content in the page.

---
+++ components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+    <div className="cont">
+        <section id="home">
+            <h2>Welcome!</h2>
+            <p>
+                Mabuhay, kababayan! Welcome to your step-by-step guide for first-time job seekers in the Philippines. Here, we make the process of securing your employment requirements simple, clear, and stress-free. Whether you’re fresh out of school or ready to chase your first career opportunity, we’ll walk together through every form, every queue, and every requirement—so you can start your professional journey with confidence. Remember, every accomplished worker began with a first step. This is yours. Padayon!
+            </p>
+        </section>
+    </div>
+    );
+}
+
+export default Home;
