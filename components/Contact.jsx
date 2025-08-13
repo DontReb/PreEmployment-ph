83af648e47df9b172f4ecca471e50ed194bac7c1
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-13 15:29:58 +0800

    Added the contact content in the page.

---
+++ components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section id="contact">
+      <div className="cont">
+        <h2>Contact</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua...
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
